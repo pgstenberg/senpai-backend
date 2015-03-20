2808b625ef27f45f30f70ffac0bbae9159e89c55
Per-Gustaf Stenberg <per-gustaf.stenberg@stonepath.se>
2015-03-20 16:19:27 +0100

    fixed column sizes

--- WebContent/app/controllers/logsController.js
+++ WebContent/app/controllers/logsController.js
@@ -10,10 +10,10 @@ var logsController = function($scope, $http) {
 	};
 	
   	$scope.logs.columnDefs = [
-  		{name:'Application',field:'application', width:'15%',cellTemplate: ' <div class="ngCellText" ng-class="col.colIndex()">&nbsp;<a href="#/application/{{ row.entity[col.field].application_id }}">{{ row.entity[col.field].applicationName }}</a><span ng-cell-text></span></div>'},
+  		{name:'Application',field:'application', width:'10%',cellTemplate: ' <div class="ngCellText" ng-class="col.colIndex()">&nbsp;<a href="#/application/{{ row.entity[col.field].application_id }}">{{ row.entity[col.field].applicationName }}</a><span ng-cell-text></span></div>'},
   		{name:'Logger',field:'logger', width:'10%'},
-    	{name:'Level',field:'level', width:'5%'},
-    	{name:"stamp",field:'stamp', width:'10%', cellTemplate: ' <div class="ngCellText" ng-class="col.colIndex()"><span ng-cell-text>&nbsp;{{ row.entity[col.field] * 1000| date:\'yyyy-MM-dd HH:mm:ss\' }}</span></div>'},
+    	{name:'Level',field:'level', width:'10%'},
+    	{name:"stamp",field:'stamp', width:'15%', cellTemplate: ' <div class="ngCellText" ng-class="col.colIndex()"><span ng-cell-text>&nbsp;{{ row.entity[col.field] * 1000| date:\'yyyy-MM-dd HH:mm:ss\' }}</span></div>'},
     	{name: 'message',field:'message'},
   	];
  
